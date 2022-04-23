28519935a16dcaee1d8213fd1480079605cd5a10
Ruben Purdy <ruben.purdy@gmail.com>
2022-04-23 15:29:39 -0400

    Updated setup

--- setup.py
+++ setup.py
@@ -8,10 +8,20 @@ setuptools.setup(
     version="0.0.1",
     author="Ruben Purdy",
     author_email="rpurdy@andrew.cmu.edu",
-    description="Run HDL simulations from python.",
+    description="Run gate-level HDL simulations from python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    classifiers=["Programming Language :: Python :: 3"],
+    url="https://github.com/circuitgraph/circuitsim",
+    project_urls={
+        "Documentation": "https://circuitgraph.github.io/circuitsim/",
+        "Source": "https://github.com/circuitgraph/circuitsim",
+    },
+    packages=["circuitsim"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires=">=3.6",
     install_requires=["circuitgraph>=0.2.0", "natsort"],
 )
